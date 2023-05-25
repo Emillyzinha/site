95bb4e02b6f0503b866602c9400b56d8cc513146
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2023-05-25 16:55:11 -0300

    Delete banco - Copia directory

--- banco - Copia/bank/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from django.urls import path
-from .views import *
-
-router = routers.SimpleRouter()
-router.register('usuario', ListarDetalharUsuario)
-router.register('clientes', ClienteViewSet)
-
-urlpatterns =[] + router.urls
