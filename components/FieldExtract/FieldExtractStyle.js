dc25f5937652dee70f3f909dcdde4989371ba0e1
Emilly <110494320+Emillyzinha@users.noreply.github.com>
2023-05-31 10:53:33 -0300

    Add files via upload

---
+++ components/FieldExtract/FieldExtractStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styleFieldsExtract = StyleSheet.create({
+    screen: {
+        flex: 1,
+        display: 'flex',
+        paddingTop: 50,
+        padding: 20,
+        alignItems: "center",
+    },
+
+})
+
+export default styleFieldsExtract
